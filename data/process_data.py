067066d5d4170b59b02c5072b30bc20c950696c5
Amar Singh <singhamarb749@gmail.com>
2020-07-07 16:56:37 +0530

    test databases were added

--- data/process_data.py
+++ data/process_data.py
@@ -68,7 +68,7 @@ def clean_data(df):
 def save_data(df, database_filename):
     
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('df', engine, index=False)  
+    df.to_sql('amar', engine, index=False)  
     pass  
 
 
